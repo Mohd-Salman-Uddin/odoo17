9ed73f60a7445ccc5e56ca59e0a68434df8a87d2
Mohd-Salman-Uddin <salman@sidmectech.in>
2024-09-02 18:04:35 +0530

    Added Dependency Of Sales & Invoicing And Modified Fields

--- my_school/models/models.py
+++ my_school/models/models.py
@@ -166,3 +166,8 @@ class InvoiceOrder(models.Model):
 
     guardian_name=fields.Char(string='Guardian Name')
     guardian_mobile=fields.Char(string='Guardian Number', size=15)
+
+class InvoiceOrderFee(models.Model):
+    _inherit = 'account.move.line'
+    fee_structure_id =fields.Many2one('school.fee.structure',string='Fee Structure')
+
